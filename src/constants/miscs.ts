14eb4ab2400db1cb1375767a98f6db447face66b
Ranjan Paudel <paudeljimrranjan@gmail.com>
2021-03-04 22:25:23 +0545

    Add some constants

---
+++ src/constants/miscs.ts
@@ -0,0 +1,4 @@
+export const ALLOWED_UNITS = ['KG', 'LTR', 'PACK'];
+
+export const DATE_REGEXP = /^\d\d\d\d-\d\d-\d\d$/;
+export const EMAIL_REGEXP = /^[a-z]([a-z]|[0-9]){2,100}@[a-z]{2,100}\.com$/;
