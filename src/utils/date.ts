9c39d3096d38ae4354de5001a0c6949df7b9892f
Ranjan Paudel <paudeljimrranjan@gmail.com>
2021-03-05 17:34:20 +0545

    Add some date and string utils

---
+++ src/utils/date.ts
@@ -0,0 +1,27 @@
+import * as moment from 'moment';
+
+import { DATE_REGEXP, STANDARD_DATE_FORMAT } from '../constants/miscs';
+
+/**
+ * Check the date format to be standard
+ * @param value String date format
+ */
+export function isStandardDateFormat(value: string) {
+  if (!DATE_REGEXP.test(value)) {
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * Check if the date is valid
+ * @param value Date
+ */
+export function isValidDate(value: string | moment.Moment) {
+  if (!moment(value, STANDARD_DATE_FORMAT).isValid()) {
+    return false;
+  }
+
+  return true;
+}
---
+++ src/utils/string.ts
@@ -0,0 +1,25 @@
+import { EMAIL_REGEXP } from '../constants/miscs';
+
+/**
+ * Check type of value to be string
+ * @param value Value to validate for string type
+ */
+export function isString(value: any) {
+  if (typeof value !== 'string') {
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * Check email format
+ * @param value Email to validate
+ */
+export function isvalidEmail(value: string) {
+  if (!EMAIL_REGEXP.test(value)) {
+    return false;
+  }
+
+  return true;
+} 
