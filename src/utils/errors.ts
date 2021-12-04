0ab94e50eb8ec1d3c7f2914bd82c07b69d3a97d5
Ranjan Paudel <paudeljimrranjan@gmail.com>
2021-12-04 17:40:42 +0545

    error_1 Add forbidden and authentication error classes

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -33,3 +33,21 @@ export class BadRequestError extends Error {
     this.code = 400;
   }
 }
+
+export class ForbiddenError extends Error {
+  code: number;
+  constructor() {
+    super("Forbidden Access!");
+
+    this.code = 403;
+  }
+}
+
+export class AuthenticationError extends Error {
+  code: number;
+  constructor(message?: string) {
+    super(message || "Authentication Failed!");
+
+    this.code = 401;
+  }
+}
